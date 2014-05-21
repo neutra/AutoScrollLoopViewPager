732d42c3a57bb5a07a26dd105dc7575347f639f7
neutra <neutra@qq.com>
2014-05-21 08:06:48 +0800

    init

---
+++ src/io/github/neutra/android/autoscrollloopviewpager/AutoScrollLoopViewPager.java
@@ -0,0 +1,254 @@
+package io.github.neutra.android.autoscrollloopviewpager;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.animation.Interpolator;
+
+import java.lang.reflect.Field;
+
+/**
+ * Auto Scroll Loop View Pager
+ * <ul>
+ * <strong>Basic Setting and Usage</strong>
+ * <li>{@link #startAutoScroll()} start auto scroll, or {@link #startAutoScroll(int)} start auto scroll delayed</li>
+ * <li>{@link #stopAutoScroll()} stop auto scroll</li>
+ * <li>{@link #setInterval(long)} set auto scroll time in milliseconds, default is {@link #DEFAULT_INTERVAL}</li>
+ * </ul>
+ * <ul>
+ * <strong>Advanced Settings and Usage</strong>
+ * <li>{@link #setDirection(int)} set auto scroll direction</li>
+ * <li>{@link #setStopScrollWhenTouch(boolean)} set whether stop auto scroll when touching, default is true</li>
+ * </ul>
+ *
+ * @author <a href="https://github.com/neutra" target="_blank">neutra</a> 2014-05-20
+ */
+public class AutoScrollLoopViewPager extends LoopViewPager {
+
+    public static final int DEFAULT_INTERVAL = 4000;
+
+    public static final int LEFT = 0;
+    public static final int RIGHT = 1;
+
+    /**
+     * auto scroll time in milliseconds, default is {@link #DEFAULT_INTERVAL} *
+     */
+    private long interval = DEFAULT_INTERVAL;
+    /**
+     * auto scroll direction, default is {@link #RIGHT} *
+     */
+    private int direction = RIGHT;
+    /**
+     * whether stop auto scroll when touching, default is true *
+     */
+    private boolean stopScrollWhenTouch = true;
+
+    private Handler handler;
+    private boolean isAutoScroll = false;
+    private boolean isStopByTouch = false;
+    private float touchX = 0f, downX = 0f;
+    private CustomDurationScroller scroller;
+
+    public static final int SCROLL_WHAT = 0;
+
+    public AutoScrollLoopViewPager(Context paramContext) {
+        super(paramContext);
+        init();
+    }
+
+    public AutoScrollLoopViewPager(Context paramContext, AttributeSet paramAttributeSet) {
+        super(paramContext, paramAttributeSet);
+        init();
+    }
+
+    private void init() {
+        handler = new MyHandler();
+        setViewPagerScroller();
+    }
+
+    /**
+     * start auto scroll, first scroll delay time is {@link #getInterval()}
+     */
+    public void startAutoScroll() {
+        isAutoScroll = true;
+        sendScrollMessage(interval);
+    }
+
+    /**
+     * start auto scroll
+     *
+     * @param delayTimeInMills first scroll delay time
+     */
+    public void startAutoScroll(int delayTimeInMills) {
+        isAutoScroll = true;
+        sendScrollMessage(delayTimeInMills);
+    }
+
+    /**
+     * stop auto scroll
+     */
+    public void stopAutoScroll() {
+        isAutoScroll = false;
+        handler.removeMessages(SCROLL_WHAT);
+    }
+
+    /**
+     * set the factor by which the duration of sliding animation will change
+     */
+    public void setScrollDurationFactor(double scrollFactor) {
+        scroller.setScrollDurationFactor(scrollFactor);
+    }
+
+    private void sendScrollMessage(long delayTimeInMills) {
+        /** remove messages before, keeps one message is running at most **/
+        handler.removeMessages(SCROLL_WHAT);
+        handler.sendEmptyMessageDelayed(SCROLL_WHAT, delayTimeInMills);
+    }
+
+    /**
+     * set ViewPager scroller to change animation duration when sliding
+     */
+    private void setViewPagerScroller() {
+        try {
+            Field scrollerField = ViewPager.class.getDeclaredField("mScroller");
+            scrollerField.setAccessible(true);
+            Field interpolatorField = ViewPager.class.getDeclaredField("sInterpolator");
+            interpolatorField.setAccessible(true);
+
+            scroller = new CustomDurationScroller(getContext(), (Interpolator) interpolatorField.get(null));
+            scroller.setScrollDurationFactor(2);
+            scrollerField.set(this, scroller);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * scroll only once
+     */
+    public void scrollOnce() {
+        PagerAdapter adapter = getAdapter();
+        int currentItem = getCurrentItem();
+        int totalCount;
+        if (adapter == null || (totalCount = adapter.getCount()) <= 1) {
+            return;
+        }
+
+        int nextItem = (direction == LEFT) ? --currentItem : ++currentItem;
+        if (nextItem < 0) {
+            setCurrentItem(-1, true);
+        } else if (nextItem == totalCount) {
+            setCurrentItem(totalCount, true);
+        } else {
+            setCurrentItem(nextItem, true);
+        }
+    }
+
+    /**
+     * <ul>
+     * if stopScrollWhenTouch is true
+     * <li>if event is down, stop auto scroll.</li>
+     * <li>if event is up, start auto scroll again.</li>
+     * </ul>
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (stopScrollWhenTouch) {
+            if (ev.getAction() == MotionEvent.ACTION_DOWN && isAutoScroll) {
+                isStopByTouch = true;
+                stopAutoScroll();
+            } else if (ev.getAction() == MotionEvent.ACTION_UP && isStopByTouch) {
+                startAutoScroll();
+            }
+        }
+
+        touchX = ev.getX();
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            downX = touchX;
+        }
+        int currentItem = getCurrentItem();
+        PagerAdapter adapter = getAdapter();
+        int pageCount = adapter == null ? 0 : adapter.getCount();
+        if (pageCount > 1) {
+            if (currentItem == 0 && downX < touchX) {
+                setCurrentItem(-1, true);
+            } else if (currentItem == pageCount - 1 && downX > touchX) {
+                setCurrentItem(pageCount, true);
+            }
+        }
+        getParent().requestDisallowInterceptTouchEvent(true);
+        return super.onTouchEvent(ev);
+    }
+
+    private class MyHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case SCROLL_WHAT:
+                    scrollOnce();
+                    sendScrollMessage(interval);
+                default:
+                    break;
+            }
+        }
+    }
+
+    /**
+     * get auto scroll time in milliseconds, default is {@link #DEFAULT_INTERVAL}
+     *
+     * @return the interval
+     */
+    public long getInterval() {
+        return interval;
+    }
+
+    /**
+     * set auto scroll time in milliseconds, default is {@link #DEFAULT_INTERVAL}
+     *
+     * @param interval the interval to set
+     */
+    public void setInterval(long interval) {
+        this.interval = interval;
+    }
+
+    /**
+     * get auto scroll direction
+     *
+     * @return {@link #LEFT} or {@link #RIGHT}, default is {@link #RIGHT}
+     */
+    public int getDirection() {
+        return (direction == LEFT) ? LEFT : RIGHT;
+    }
+
+    /**
+     * set auto scroll direction
+     *
+     * @param direction {@link #LEFT} or {@link #RIGHT}, default is {@link #RIGHT}
+     */
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    /**
+     * whether stop auto scroll when touching, default is true
+     *
+     * @return the stopScrollWhenTouch
+     */
+    public boolean isStopScrollWhenTouch() {
+        return stopScrollWhenTouch;
+    }
+
+    /**
+     * set whether stop auto scroll when touching, default is true
+     *
+     * @param stopScrollWhenTouch
+     */
+    public void setStopScrollWhenTouch(boolean stopScrollWhenTouch) {
+        this.stopScrollWhenTouch = stopScrollWhenTouch;
+    }
+}
